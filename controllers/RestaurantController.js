be373b9c6473a5324ce46a224fcf73918682786c
saiviru <saikiran@nerdisnerd.com>
2023-05-08 19:47:35 +0530

    change in sending response1

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -9,16 +9,18 @@ const RestaurantController = async (req, res, next) => {
       const order = new Restaurant({ userId, items, restaurantId, totalAmount });
   
       // Save the new order document to the database
-      await order.save();
-  
-      // Return the newly created order as the response
-      res.status(201).send({
-				message: 'Order Created Successfully',
-				order,
-			});
-    } catch (error) {
-      next(error);
-    }
+      await order.save().then((result) => {
+        res.status(201).send({
+          message: 'Order Created Successfully',
+          result,
+        });
+      })
+      .catch((error) => {
+        res.status(500).send({
+          message: 'Error creating the order',
+          error,
+        });
+      });
   };
 
   export {RestaurantController}
